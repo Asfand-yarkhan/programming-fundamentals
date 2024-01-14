da3dc0c303a24adf8ba3b440eff04892465b8212
Asfand Yar Khan <156542576+Asfand-yarkhan@users.noreply.github.com>
2024-01-14 17:33:06 +0500

    Power rule
    It is a method to find power by recursive function by using c++.

---
+++ power.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int power(int num, int x)
+{
+	if(x==0)
+	{
+		return 1;
+	}
+	else if(x==1)
+	{
+		return num;
+	}
+	else 
+	{
+		return num * power( num, x-1);
+	}
+	
+}
+int main()
+{
+	int a,b;
+	cout<<"ENTER THE NUMBER: "<<endl;
+	cin>>a;
+	cout<<"ENTER THE POWER: "<<endl;
+	cin>>b;
+	cout<<endl;
+	cout<<a<<" ^ "<<b<<" = "<<power(a,b);
+	return 0;
+}
